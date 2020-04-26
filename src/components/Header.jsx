c421cb33c628dd4d8708af580d469b049c2467a6
manish465 <ms5978307@gmail.com>
2020-04-26 12:08:34 -0700

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { AppBar, makeStyles, Toolbar } from "@material-ui/core";
-import logoImg from "../assets/logo/logo.svg";
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import logoImg from "../assets/logo/logo.png";
 
-const useStyles = makeStyles({ appBar: { background: "transparent" } });
+const useStyles = makeStyles({
+    appBar: { background: "transparent" },
+    logo: { margin: "10px" },
+});
 
 const Header = () => {
     const classes = useStyles();
@@ -10,7 +13,8 @@ const Header = () => {
     return (
         <AppBar position='static' variant='outlined' className={classes.appBar}>
             <Toolbar>
-                <img src={logoImg} alt='Logo' />
+                <img className={classes.logo} src={logoImg} alt='Logo' />
+                <Typography variant='h4'>Microsoft</Typography>
             </Toolbar>
         </AppBar>
     );
