29f39cdb3f75567270a813d477cb7473a66cf433
manish465 <ms5978307@gmail.com>
2020-04-27 15:36:47 -0700

    added download button

--- src/components/Pages/Apps/Cards.jsx
+++ src/components/Pages/Apps/Cards.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 import data from "./data";
 
-import { IconButton, makeStyles, Grid, Typography } from "@material-ui/core";
+import {
+    Fab,
+    makeStyles,
+    Grid,
+    Typography,
+    Backdrop,
+    CircularProgress,
+} from "@material-ui/core";
+import GetAppIcon from "@material-ui/icons/GetApp";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     card: {
         padding: "20px",
         marginBottom: "20px",
@@ -18,33 +26,57 @@ const useStyles = makeStyles({
         marginTop: "20px",
         fontFamily: "Yu Gothic",
     },
-});
+    fab: {
+        background: "green",
+        marginLeft: "80px",
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        background: "rgb(0,0,0,0.1)",
+    },
+}));
 
 const Cards = () => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleToggle = () => {
+        setOpen(!open);
+    };
 
     const card = data.map((item, key) => (
-        <IconButton>
-            <Grid container className={classes.card} key={key}>
-                <Grid item>
-                    <img src={item.imgUrl} alt='' />
-                </Grid>
-                <Grid>
-                    <Typography
-                        style={{ fontWeight: "bold" }}
-                        className={classes.text}
-                        variant='h5'>
-                        {item.name}
-                    </Typography>
-                    <Typography className={classes.text} variant='h5'>
-                        {item.downloads}
-                    </Typography>
-                    <Typography className={classes.text} variant='h5'>
-                        free
-                    </Typography>
-                </Grid>
+        <Grid container className={classes.card} key={key}>
+            <Grid item>
+                <img src={item.imgUrl} alt='' />
             </Grid>
-        </IconButton>
+            <Grid>
+                <Typography
+                    style={{ fontWeight: "bold" }}
+                    className={classes.text}
+                    variant='h5'>
+                    {item.name}
+                </Typography>
+                <Typography className={classes.text} variant='h5'>
+                    {item.downloads}
+                </Typography>
+                <Typography className={classes.text} variant='h5'>
+                    free
+                    <Fab className={classes.fab} onClick={handleToggle}>
+                        <GetAppIcon color='primary' />
+                    </Fab>
+                    <Backdrop
+                        className={classes.backdrop}
+                        open={open}
+                        onClick={handleClose}>
+                        <CircularProgress variant='indeterminate' />
+                    </Backdrop>
+                </Typography>
+            </Grid>
+        </Grid>
     ));
 
     return (
