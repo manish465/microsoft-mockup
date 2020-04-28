1c437f2fc896054fb2cbc1275b8f3e08f5818c1b
manish465 <ms5978307@gmail.com>
2020-04-28 14:50:49 -0700

    added button

--- src/components/Pages/Computers/Buttons.jsx
+++ src/components/Pages/Computers/Buttons.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Grid, Button } from "@material-ui/core";
+import MouseIcon from "@material-ui/icons/Mouse";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+
 const Buttons = () => {
-    return <div>Button</div>;
+    return (
+        <Grid justify='space-around' container style={{ marginTop: "30px" }}>
+            <Grid item>
+                <Button
+                    size='large'
+                    style={{ color: "#005A95" }}
+                    startIcon={<MouseIcon />}>
+                    ACCESSORIES
+                </Button>
+            </Grid>
+            <Grid item>
+                <Button
+                    size='large'
+                    style={{ color: "#005A95" }}
+                    startIcon={<PhoneAndroidIcon />}>
+                    SYNC YOUR PHONE
+                </Button>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default Buttons;
