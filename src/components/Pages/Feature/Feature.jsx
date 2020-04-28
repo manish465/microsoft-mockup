150db6fd9fc04c456e3cd69d01f11a36c666abee
manish465 <ms5978307@gmail.com>
2020-04-28 12:10:59 -0700

    added background to feature.jsx

--- src/components/Pages/Feature/Feature.jsx
+++ src/components/Pages/Feature/Feature.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import Header from "../../Header/Header";
 
+import bgImg from "../../../assets/feature/bgImg.png";
+
+import { Box, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    feature: {
+        height: "2435px",
+        backgroundImage: `url(${bgImg})`,
+        backgroundPositionY: "340px",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+    },
+});
+
 const Feature = () => {
-    return <Header />;
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.feature}>
+            <Header />
+        </Box>
+    );
 };
 
 export default Feature;
