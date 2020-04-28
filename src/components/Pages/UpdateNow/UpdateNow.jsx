e34b4506d3b51bd6039fa8af8aa5b756f8c5bd1e
manish465 <ms5978307@gmail.com>
2020-04-27 21:51:02 -0700

    added banner text to update page

--- src/components/Pages/UpdateNow/UpdateNow.jsx
+++ src/components/Pages/UpdateNow/UpdateNow.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import Header from "../../Header/Header";
 
+import bgImg from "../../../assets/update/Background.png";
+
+import { Box, makeStyles, Grid, Typography, Button } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    update: {
+        backgroundImage: `url(${bgImg})`,
+        height: "100vh",
+    },
+    headingText: {
+        fontFamily: "Segoe UI",
+        fontWeight: "bolder",
+    },
+});
+
 const UpdateNow = () => {
-    return <Header />;
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.update}>
+            <Header />
+            <Grid container justify='space-around' alignItems='center'>
+                <Grid item>
+                    <Typography
+                        variant='h1'
+                        className={classes.headingText}
+                        color='primary'>
+                        What New
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Button
+                        style={{ background: "#00367D" }}
+                        variant='contained'>
+                        <Typography color='primary'>Update now</Typography>
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 };
 
 export default UpdateNow;
