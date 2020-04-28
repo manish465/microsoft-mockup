accd4eec59cd98b8b009d1386cee5f85a5a01e27
manish465 <ms5978307@gmail.com>
2020-04-28 15:29:11 -0700

    added data and cards to computer.jsx

--- src/components/Pages/Computers/Cards.jsx
+++ src/components/Pages/Computers/Cards.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
+import data from "./data";
+
+import { Card, makeStyles, CardMedia } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    cards: { marginTop: "50px" },
+    cardRight: {
+        position: "relative",
+        left: "35%",
+        borderRadius: "24px",
+        background: "#E5E5E5",
+        width: "1200px",
+        height: "430px",
+        marginBottom: "40px",
+    },
+    cardLeft: {
+        position: "relative",
+        left: "5%",
+        borderRadius: "24px",
+        background: "#E5E5E5",
+        width: "1200px",
+        height: "430px",
+        marginBottom: "30px",
+    },
+});
+
 const Cards = () => {
-    return <div>Cards</div>;
+    const classes = useStyles();
+    const Cards = data.map((card, key) => (
+        <Card
+            className={card.left ? classes.cardLeft : classes.cardRight}
+            variant='outlined'>
+            <CardMedia
+                style={{ width: "360px" }}
+                component='img'
+                image={card.imgUrl}
+                alt=''
+            />
+        </Card>
+    ));
+
+    return <div className={classes.cards}>{Cards}</div>;
 };
 
 export default Cards;
---
+++ src/components/Pages/Computers/data.js
@@ -0,0 +1,53 @@
+import img1 from "../../../assets/computers/data/Surface_Home_Mosaic_Block1_image1_V3.png";
+import img2 from "../../../assets/computers/data/Surface_Home_Mosaic_Block1_image2_V3.png";
+import img3 from "../../../assets/computers/data/Surface_Home_Mosaic_Block1_image3_NSS_V3.png";
+import img4 from "../../../assets/computers/data/Surface_Home_Mosaic_Block1_image4_V3.png";
+import img5 from "../../../assets/computers/data/Surface_Home_Mosaic_Block3_image1_V3.png";
+import img6 from "../../../assets/computers/data/Surface_Home_Mosaic_Block3_image2_ACC_V3.png";
+
+const data = [
+    {
+        name: "Surface Go",
+        left: true,
+        label: "Smallest lightest Surface with touchscreen",
+        linkText: "MEET SURFACE GO",
+        imgUrl: img1,
+    },
+    {
+        name: "Surface Pro 7",
+        left: false,
+        label: "Smallest lightest Surface with touchscreen",
+        linkText: "MEET SURFACE PRO 7",
+        imgUrl: img2,
+    },
+    {
+        name: "Surface Laptop 3",
+        left: true,
+        label: "Smallest lightest Surface with touchscreen",
+        linkText: "MEET SURFACE LAPTOP 3",
+        imgUrl: img3,
+    },
+    {
+        name: "Surface Book 2",
+        left: false,
+        label: "Smallest lightest Surface with touchscreen",
+        linkText: "MEET SURFACE BOOK 2",
+        imgUrl: img4,
+    },
+    {
+        name: "Surface Pro X",
+        left: true,
+        label: "Smallest lightest Surface with touchscreen",
+        linkText: "MEET SURFACE PRO X",
+        imgUrl: img5,
+    },
+    {
+        name: "Surface Home",
+        left: false,
+        label: "Smallest lightest Surface with touchscreen",
+        linkText: "MEET SURFACE HOME",
+        imgUrl: img6,
+    },
+];
+
+export default data;
