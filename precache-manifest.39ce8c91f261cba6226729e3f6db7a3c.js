41f46ffac7d99f472766c5208578ed2c7e24e7ba
manish465 <ms5978307@gmail.com>
2020-04-28 17:05:12 -0700

    Updates

---
+++ precache-manifest.39ce8c91f261cba6226729e3f6db7a3c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08d4bba6be296f8a6cdd7a8ea286456",
+    "url": "/microsoft-mockup/index.html"
+  },
+  {
+    "revision": "d77d5e45fa1c631c817a",
+    "url": "/microsoft-mockup/static/css/main.abaadb21.chunk.css"
+  },
+  {
+    "revision": "0daa78f88b627fef7aa3",
+    "url": "/microsoft-mockup/static/js/2.f115b878.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/microsoft-mockup/static/js/2.f115b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77d5e45fa1c631c817a",
+    "url": "/microsoft-mockup/static/js/main.8eff6188.chunk.js"
+  },
+  {
+    "revision": "166df6dda38db3033e7e",
+    "url": "/microsoft-mockup/static/js/runtime-main.7e361710.js"
+  },
+  {
+    "revision": "e7bc4292f3ea1a48ce714dbb80f23b74",
+    "url": "/microsoft-mockup/static/media/Background.e7bc4292.png"
+  },
+  {
+    "revision": "f86da796be00f93e71749add289d2ec8",
+    "url": "/microsoft-mockup/static/media/Background.f86da796.png"
+  },
+  {
+    "revision": "5972900b2edb696c01f6709a1d61ff8e",
+    "url": "/microsoft-mockup/static/media/RE4np58.5972900b.png"
+  },
+  {
+    "revision": "3e6bfc2dbc1db5ac48aa3cbbf0e7ee15",
+    "url": "/microsoft-mockup/static/media/RE4u1kQ.3e6bfc2d.png"
+  },
+  {
+    "revision": "0f111c23cdd387f4acb42d0cdc9bafe0",
+    "url": "/microsoft-mockup/static/media/RE4u6wn.0f111c23.png"
+  },
+  {
+    "revision": "25653f43f249da7388c133b5d4e6d217",
+    "url": "/microsoft-mockup/static/media/RE4u99y.25653f43.png"
+  },
+  {
+    "revision": "bcff3b7cb8c2d3354dc8f8c2d2b6e80f",
+    "url": "/microsoft-mockup/static/media/RE4ubMP.bcff3b7c.png"
+  },
+  {
+    "revision": "f2a30910465535ef893d898c2fbbfece",
+    "url": "/microsoft-mockup/static/media/Surface_Home_Mosaic_Block1_image1_V3.f2a30910.png"
+  },
+  {
+    "revision": "c78677890491b8879f8aef0937f63bfb",
+    "url": "/microsoft-mockup/static/media/Surface_Home_Mosaic_Block1_image2_V3.c7867789.png"
+  },
+  {
+    "revision": "12b54fee4cf8eca3c1b065d45e1211c5",
+    "url": "/microsoft-mockup/static/media/Surface_Home_Mosaic_Block1_image3_NSS_V3.12b54fee.png"
+  },
+  {
+    "revision": "6e3859f5da4d26b02d875c482b3d8caf",
+    "url": "/microsoft-mockup/static/media/Surface_Home_Mosaic_Block1_image4_V3.6e3859f5.png"
+  },
+  {
+    "revision": "0cd6901610577f4ac05a58685941ee6d",
+    "url": "/microsoft-mockup/static/media/Surface_Home_Mosaic_Block3_image1_V3.0cd69016.png"
+  },
+  {
+    "revision": "547f3da4f7c4bc4fa61d0c3c628166a0",
+    "url": "/microsoft-mockup/static/media/Surface_Home_Mosaic_Block3_image2_ACC_V3.547f3da4.png"
+  },
+  {
+    "revision": "f35b12e8ce2c241f79aa39dcfbc22ba8",
+    "url": "/microsoft-mockup/static/media/bannerBg.f35b12e8.png"
+  },
+  {
+    "revision": "45dd69997b4ad76501b3e34bfec7ca32",
+    "url": "/microsoft-mockup/static/media/bg.45dd6999.png"
+  },
+  {
+    "revision": "a256a98541d0b1246a0a35533e751e4b",
+    "url": "/microsoft-mockup/static/media/bg.a256a985.png"
+  },
+  {
+    "revision": "d87a8807a7a82ce3febd37fa9ba226db",
+    "url": "/microsoft-mockup/static/media/bgImg.d87a8807.png"
+  },
+  {
+    "revision": "a0620ea7a8d1145231370067bf02b082",
+    "url": "/microsoft-mockup/static/media/img1.a0620ea7.png"
+  },
+  {
+    "revision": "e5491ffe6794463e6fbf0b7edfab5c85",
+    "url": "/microsoft-mockup/static/media/img3.e5491ffe.png"
+  },
+  {
+    "revision": "880353f9f88d118e7c735452d2a20fef",
+    "url": "/microsoft-mockup/static/media/img4.880353f9.png"
+  },
+  {
+    "revision": "0c9703274fa53e8c0826f578dd7be90d",
+    "url": "/microsoft-mockup/static/media/img6.0c970327.png"
+  },
+  {
+    "revision": "baf82a2db5b08f10321112c525fd9efd",
+    "url": "/microsoft-mockup/static/media/img7.baf82a2d.png"
+  }
+]);
